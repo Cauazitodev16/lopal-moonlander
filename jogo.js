d94a78aafe754d634d16f6a8e5c90fa0505fae58
Cauazitodev16 <rubislinscaua@gmail.com>
2025-03-28 08:58:18 -0300

    Segundo commit

--- jogo.js
+++ jogo.js
@@ -100,7 +100,7 @@ function desenhar(){
   animarFoguete(); */
 
 
-let yModulo = 400;    // Posição inicial do módulo lunar
+/*let yModulo = 400;    // Posição inicial do módulo lunar
 let velocidade = 3;  // Velocidade com que o módulo sobe
 
 
@@ -204,7 +204,105 @@ function teclaSolta(evento){
         yModulo.motorLigado = false
     }
 }
-animarModuloLunar();
+animarModuloLunar();*/
 
 
 //o programa comentado lá no inicio é um outro modelo de nave
+let moduloLunar = {
+  posicao: {
+      x: 100,
+      y: 100
+  },
+  angulo: 0,
+  largura: 20,
+  altura: 20,
+  cor: "black",
+  motorLigado: false,
+  velocidade: {
+      x: 0,
+      y: 0
+  }
+}
+
+function desenharModuloLunar(){
+  contexto.save();
+  contexto.beginPath();
+  contexto.translate(moduloLunar.posicao.x, moduloLunar.posicao.y);
+  contexto.rotate(moduloLunar.angulo);
+  contexto.rect(moduloLunar.largura * -0.5, moduloLunar.altura * -0.5,
+       moduloLunar.largura, moduloLunar.altura);
+  contexto.fillStyle = moduloLunar.cor;
+  contexto.fill();
+  contexto.closePath();
+
+  if(moduloLunar.motorLigado){
+      desenharChama();
+  }
+  
+  contexto.restore();
+}
+
+function desenharChama(){
+  contexto.beginPath();
+  contexto.moveTo(moduloLunar.largura * -0.5, moduloLunar.altura * 0.5);
+  contexto.lineTo(moduloLunar.largura * 0.5, moduloLunar.altura * 0.5);
+  //Determina o tamanho da chama
+  contexto.lineTo(0, moduloLunar.altura * 0.5 + Math.random() * 10 );
+  contexto.lineTo(moduloLunar.largura * -0.5, moduloLunar.altura * 0.5);
+  contexto.closePath();
+  contexto.fillStyle = "orange";
+  contexto.fill();
+}
+
+
+
+
+let x = 100;
+
+function desenhar(){
+  //limpar a tela
+  contexto.clearRect(0, 0, canvas.width, canvas.height);
+
+  contexto.save() //salvando contexto atual
+  //vai (muda o contexto) para o centro da tela 
+  contexto.translate(canvas.width / 2, canvas.height / 2 );
+  contexto.beginPath();
+  //contexto.arc(x, 100, 25, 0, 2 * Math.PI);
+  contexto.rotate(Math.PI/4);
+  contexto.rect(x, 100, 25, 10);
+  contexto.fillStyle = "black";
+  contexto.fill();
+  contexto.restore(); //restaura o contexto anterior
+
+  x = x + 1;
+  atracaoGravitacional();
+  desenharModuloLunar();
+  requestAnimationFrame(desenhar);
+  
+}
+
+//Pressionando a seta para cima para ligar o motor
+document.addEventListener("keydown", teclaPressionada);
+function teclaPressionada(evento){
+ if(evento.keyCode == 38){
+      moduloLunar.motorLigado = true;
+  }
+}
+//Soltando a seta para cima para desligar o motor
+document.addEventListener("keyup", teclaSolta);
+function teclaSolta(evento){
+  if(evento.keyCode == 38){
+      moduloLunar.motorLigado = false;
+  }
+}
+
+let gravidade = 0.1;
+function atracaoGravitacional(){
+  moduloLunar.posicao.x += moduloLunar.velocidade.x;
+  moduloLunar.posicao.y += moduloLunar.velocidade.y;
+  if(moduloLunar.motorLigado){
+      moduloLunar.velocidade.y -= 0.2;
+  }
+  moduloLunar.velocidade.y += gravidade;
+}
+desenhar();
